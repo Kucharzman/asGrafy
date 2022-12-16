6eed62533012e1d0acea4a2134f656dab9dc6eee
test <test@asd.pl>
2022-12-16 10:58:35 +0100

    2.0.0

--- app/src/main/java/com/example/asgrafy/MainActivity.kt
+++ app/src/main/java/com/example/asgrafy/MainActivity.kt
@@ -2,13 +2,31 @@ package com.example.asgrafy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //deklaracja grafu
+        val arrGreg = arrayOf(
+                   //A,B,C,D,E,F,G
+            arrayOf( 0,0,0,0,0,0,0 ),//A
+            arrayOf( 0,0,0,0,0,0,0 ),//B
+            arrayOf( 0,0,0,0,0,0,0 ),//C
+            arrayOf( 0,0,0,0,0,0,0 ),//D
+            arrayOf( 0,0,0,0,0,0,0 ),//E
+            arrayOf( 0,0,0,0,0,0,0 ),//F
+            arrayOf( 0,0,0,0,0,0,0 ),//G
+        )
 
-
+        /*
+        for (row in arrGreg){
+            for(col in arrGreg){
+                findViewById<TextView>(R.id.tvOutPut).append(arrGreg[row][col].toString())
+            }
+        }
+         */
     }
 }
