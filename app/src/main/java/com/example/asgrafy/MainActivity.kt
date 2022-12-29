1d2f119baf22b8f1a5a276a8b06f224f1b28f17a
Dejwa <dawid.kubanek.31@gmail.com>
2022-12-29 19:49:31 +0100

    2.1.0 wyświetlanie połączeń wierzchołków

--- app/src/main/java/com/example/asgrafy/MainActivity.kt
+++ app/src/main/java/com/example/asgrafy/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.asgrafy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +12,46 @@ class MainActivity : AppCompatActivity() {
 
         //deklaracja grafu
         val arrGreg = arrayOf(
-                   //A,B,C,D,E,F,G
-            arrayOf( 0,0,0,0,0,0,0 ),//A
-            arrayOf( 0,0,0,0,0,0,0 ),//B
-            arrayOf( 0,0,0,0,0,0,0 ),//C
-            arrayOf( 0,0,0,0,0,0,0 ),//D
-            arrayOf( 0,0,0,0,0,0,0 ),//E
-            arrayOf( 0,0,0,0,0,0,0 ),//F
-            arrayOf( 0,0,0,0,0,0,0 ),//G
+                   //A,B,C,D,E,F
+            arrayOf( 0,1,0,0,0,0 ),//A
+            arrayOf( 0,0,0,0,0,0 ),//B
+            arrayOf( 0,0,0,0,0,0 ),//C
+            arrayOf( 0,0,0,1,0,0 ),//D
+            arrayOf( 0,0,0,0,0,1 ),//E
+            arrayOf( 0,0,0,0,0,0 ) //F
         )
+
+        findViewById<Button>(R.id.bttGraph).setOnClickListener {
+
+            findViewById<TextView>(R.id.tvOutPut).text = ""
+            var cTx = ""
+            var rTx = ""
+
+            for (r in 0..5){
+                for (c in 0..5){
+                    if (arrGreg[r][c] == 1){
+                        when(r){
+                            0 -> cTx = "A"
+                            1 -> cTx = "B"
+                            2 -> cTx = "C"
+                            3 -> cTx = "D"
+                            4 -> cTx = "E"
+                            5 -> cTx = "F"
+                        }
+                        when(c){
+                            0 -> rTx = "A"
+                            1 -> rTx = "B"
+                            2 -> rTx = "C"
+                            3 -> rTx = "D"
+                            4 -> rTx = "E"
+                            5 -> rTx = "F"
+                        }
+
+                        findViewById<TextView>(R.id.tvOutPut).append("Połączenie " + rTx + " z " + cTx +"\n")
+                    }
+                }
+            }
+        }
 
         /*
         for (row in arrGreg){
