5b0d7248ce9121f4142179f5547d6192aeb585e2
Dejwa <dawid.kubanek.31@gmail.com>
2023-01-07 12:12:54 +0100

    2.5.0 - algorytm dodany

--- app/src/main/java/com/example/asgrafy/MainActivity.kt
+++ app/src/main/java/com/example/asgrafy/MainActivity.kt
@@ -166,6 +166,47 @@ class MainActivity : AppCompatActivity() {
         bttDijk.setOnClickListener {
 
 
+        }//bttDijkstra
+
+        data class Graph<T>(
+            val vertices: Set<T>,
+            val edges: Map<T,Set<T>>,
+            val weights: Map<Pair<T,T>,Int>
+        )
+
+        fun <T> dijkstra(graph: Graph<T>, startPoint: T): Map<T,T?>{
+            val subSet: MutableSet<T> = mutableSetOf()
+            val delta = graph.vertices.map { it to Int.MAX_VALUE }.toMap().toMutableMap()
+            delta[startPoint] = 0
+
+            val previous: MutableMap<T,T?> = graph.vertices.map { it to null }.toMap().toMutableMap()
+
+            while (subSet != graph.vertices) {
+                val v: T = delta
+                    .filter { !subSet.contains(it.key) }
+                    .minBy { it.value }!!
+                    .key
+
+                graph.edges.getValue(v).minus(subSet).forEach { neighbor ->
+                    val newPath = delta.getValue(v) + graph.weights.getValue(Pair(v,neighbor))
+
+                    if (newPath < delta.getValue(neighbor)){
+                        delta[neighbor] = newPath
+                        previous[neighbor] = v
+                    }
+                }
+
+                subSet.add(v)
+            }
+            return previous.toMap()
+        }
+
+        fun <T> shortestPath(shortestPathTree: Map<T, T?>, start: T, end: T): List<T> {
+            fun pathTo(start: T, end: T): List<T> {
+                if (shortestPathTree[end] == null) return listOf(end)
+                return listOf(pathTo(start, shortestPathTree[end]!!), listOf(end)).flatten()
+            }
+            return pathTo(start, end)
         }
 
     }//OnCreate
